63a74d7add89dff67b3a48d4b494e1bd4f66912e
kamya-creator <71540845+kamya-creator@users.noreply.github.com>
2025-05-26 22:25:20 +0530

    LeetCode discussion

--- Level2 - Level UP Bro/Sudoku Solver/LeetCode37SudokuSolver.java
+++ Level2 - Level UP Bro/Sudoku Solver/LeetCode37SudokuSolver.java
@@ -1,5 +1,34 @@
 package classicProblms;
 
+/*
+# Intuition
+Tarvese row wise , give chance to every cell to get filled.
+Options to fill an empty cell(i.e board[i][j] == '.') are from 1 to 9 i.e 1,2,3,....9.
+If u reaches a cell that is already filled then pass control to next ,cell.
+#### How to Calculate next cell??
+**Case 1:** Happy case, you are at within range of current row - column i.e u have not at extreme of row, then just pass control to next immediate cell ---> nextI = i , nextJ = j + 1
+**Case 2:** Edge Case --- U are at last column of current row , ab control next row ek 0th column ko pass krna h so, 
+nextI = i + 1, nextJ = 0; 
+
+### How to check for validity???
+1> Row validity - Simple traverse current row and check if value is present then return false.
+2> Column validity --- Simple traverse current column and check if value is present then return false.
+3> SubGrid vailidity --- I have not used mathematical formula for this, I have used my obervation, dry run once u will get initution.
+Hint: [0,1,2] rows have sourceRow as 0
+    [3,4,5] row have sourceRow as 3
+    [6,7,8] row have sourceRow as 6
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+https://leetcode.com/problems/sudoku-solver/solutions/6783679/java-solution-with-explaination-by-kamya-mhcv/
+*/
 public class LeetCode37SudokuSolver {
     public static void main(String[] args) {
         char[][] board = {
