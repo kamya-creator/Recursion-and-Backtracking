9548ff0d7bc7e002295ed7407bbbe2273d949b60
kamya-creator <71540845+kamya-creator@users.noreply.github.com>
2025-05-29 15:57:25 +0530

    FriendPairing Problem with 2 Approaches

--- Level2 - Level UP Bro/FriendPairing Prroblem/FriendPairing.java
+++ Level2 - Level UP Bro/FriendPairing Prroblem/FriendPairing.java
@@ -4,13 +4,56 @@ import java.util.ArrayList;
 
 public class FriendPairing {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 4;
         ArrayList<ArrayList<Integer>> partitions = new ArrayList<>();
         for(int i = 0;i<n;i++)
         {
             partitions.add(new ArrayList<>());
         }
         solvewithKPartitions(1, n, partitions);
+
+
+
+        boolean[] visited = new boolean[n + 1];
+        String pairingSoFar = "";
+        solveWithVistedArray(1, n, pairingSoFar, visited);
+    }
+
+    public static  void solveWithVistedArray(int level, int n , String pairingSoFar, boolean[] visited)
+    {
+
+
+        if(level > n)
+        {
+            System.out.println(pairingSoFar);
+            return;
+        }
+
+        if(visited[level] == true)
+        {
+            solveWithVistedArray(level + 1, n , pairingSoFar, visited);
+        }
+        else if(visited[level] == false)
+        {
+            // stay single ki call
+            visited[level] = true;
+            solveWithVistedArray(level + 1, n, pairingSoFar + "(" + level+")", visited);
+            visited[level] = false;
+
+            // pair up ki call
+            for(int j = level + 1;j<=n;j++)
+            {
+                if(visited[j] == false)
+                {
+                    visited[level] = true;
+                    visited[j] = true;
+                    solveWithVistedArray(level + 1, n, pairingSoFar + "(" + level +"-"+ j+")", visited);
+                    visited[level] = false;
+                    visited[j] = false;
+                }
+            }
+
+        }
     }
     public static void solvewithKPartitions(int level, int n , ArrayList<ArrayList<Integer>> partitions)
     {
