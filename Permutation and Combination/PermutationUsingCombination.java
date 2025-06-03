2271cf5d79f29640823e4d25b8713eb6dd30448d
kamya-creator <71540845+kamya-creator@users.noreply.github.com>
2025-06-03 15:52:08 +0530

    Update PermutationUsingCombination.java

--- Permutation and Combination/PermutationUsingCombination.java
+++ Permutation and Combination/PermutationUsingCombination.java
@@ -6,15 +6,15 @@ public class PermutationUsingCombination {
     public static void main(String[] args) {
         int[] boxes = new int[3];
         int[] item = new int[2];
-        solve(boxes, 0, 1,1,item);
+        solve(boxes, 0, 0,item);
     }
-    public static void solve(int[] boxes, int level, int currItem, int item, int[] totalItem)
+    public static void solve(int[] boxes, int level, int itemSelectedSoFar, int[] totalItem)
     {
 
 
         if(level >= boxes.length)
         {
-            if(item > totalItem.length)
+            if(itemSelectedSoFar == totalItem.length)
             {
                 System.out.println(Arrays.toString(boxes));
             }
@@ -22,7 +22,7 @@ public class PermutationUsingCombination {
         }
 
         // no ki call
-        solve(boxes, level + 1, currItem, item, totalItem);
+        solve(boxes, level + 1, itemSelectedSoFar, totalItem);
 
         // yes ki call with arrangement
         for(int i = 0;i<totalItem.length;i++)
@@ -30,7 +30,7 @@ public class PermutationUsingCombination {
             if(totalItem[i] == 0) {
                 boxes[level] = i+1;
                 totalItem[i] = i+1;
-                solve(boxes, level +1, currItem , item + 1, totalItem);
+                solve(boxes, level +1 , itemSelectedSoFar + 1, totalItem);
                 boxes[level] = 0;
                 totalItem[i] =0;
             }
